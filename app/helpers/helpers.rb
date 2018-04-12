9d58cd1a4ba2ced4ea3a3bdcc7bc616829840e79
Chaim Reisman <crreisman@gmail.com>
2018-04-12 16:58:45 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -2,4 +2,12 @@ class Helpers
   def self.current_user(session)
     User.find_by(id: session[:user_id])
   end
+
+  def self.is_logged_in(session)
+    if session[:user_id]
+      true 
+    else 
+      false 
+    end 
+  end 
 end
